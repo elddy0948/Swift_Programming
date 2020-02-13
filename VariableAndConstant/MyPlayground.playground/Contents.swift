24b91a8d02f2cd2ee2c4e424e58fdf1d8f325214
김호준 <elddy0948@gmail.com>
2020-02-13 15:44:21 +0900

    Variable And Constant

---
+++ VariableAndConstant/MyPlayground.playground/Contents.swift
@@ -0,0 +1,20 @@
+import Swift
+
+// var [변수명]: [데이터타입] = [값]
+var myName: String = "Holuck"
+
+// var [변수명] = [값]
+var age = 25
+
+age = 1 // age == 1
+
+
+//let [변수명]: [데이터타입] = [값]
+let constName: String = "Holuck"
+
+
+//let [변수명] = [값]
+let constAge = 25
+
+age = 99 // age==99
+//constAge = 99 // error!
